6aecdb8330409101fd4f47ec886ee5e88d729409
DennisP <poba_jaria@hotmail.com>
2023-11-29 14:11:35 +0100

    view change Jaria

--- FinalProject/resources/views/welcome.blade.php
+++ FinalProject/resources/views/welcome.blade.php
@@ -1,8 +1,7 @@
-<<<<<<< HEAD
 <h1>this was from Yaz's Branch</h1><p>An other change</p>
-=======
+
 <h1>Welcome View!</h1>
 <p>THis is change</p>
 <p>Merge Modifications</p>
 <p>Changenemt de merde</p>
->>>>>>> 98ba01efc7cf6e1a3e25d4edddcf945ccd6c17b5
+
