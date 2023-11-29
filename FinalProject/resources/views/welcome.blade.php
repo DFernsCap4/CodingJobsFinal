d1282713d20154cf57d88b16b2d592fb8e357429
Wassim <wasssim.abuzeid@gmail.com>
2023-11-29 14:18:26 +0100

    commit4

--- FinalProject/resources/views/welcome.blade.php
+++ FinalProject/resources/views/welcome.blade.php
@@ -4,10 +4,6 @@
 <p>THis is change</p>
 <p>Merge Modifications</p>
 <p>Changenemt de merde</p>
-<<<<<<< HEAD
-
-=======
 <p>hello i am here</p>
 <p>hello i am here</p>hsgdjejld
 <h1>this was from Yaz's Branch</h1>
->>>>>>> a1e8ca1c65db8f35578a90e32adfc386b73c77f9
