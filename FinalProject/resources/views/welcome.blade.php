eccc5496640f3c85716d87f64fd50f99c782e8f2
Fin <finlay.arthur@gmail.com>
2023-11-29 14:10:35 +0100

    renamed:    Images/Y5Xj1pIdrHbJl61-YQrKkqYCdwS8Jxk6sN7hvN_aHIw.webp -> FinalProject/Y5Xj1pIdrHbJl61-YQrKkqYCdwS8Jxk6sN7hvN_aHIw.webp 	modified:   FinalProject/resources/views/welcome.blade.php

--- FinalProject/resources/views/welcome.blade.php
+++ FinalProject/resources/views/welcome.blade.php
@@ -2,3 +2,5 @@
 
 //this is ahmad talk to me from here guys
 <a href="https://chat.openai.com/">talk to me</a><img src="../../../Images/Y5Xj1pIdrHbJl61-YQrKkqYCdwS8Jxk6sN7hvN_aHIw.webp">
+
+<img src="../../Y5Xj1pIdrHbJl61-YQrKkqYCdwS8Jxk6sN7hvN_aHIw.webp">
