0da4c22f785550af4cb2b5a88844ea58273739d4
la-viza <97736137+la-viza@users.noreply.github.com>
2023-11-29 14:26:01 +0100

    created view

---
+++ FinalProject/resources/views/luiza.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <p>Александр Блок
+Ночь, улица, фонарь, аптека… (отрывок из цикла «Пляски смерти»)
+Ночь, улица, фонарь, аптека,
+Бессмысленный и тусклый свет.
+Живи еще хоть четверть века —
+Всё будет так. Исхода нет.
+Умрёшь — начнёшь опять сначала
+И повторится всё, как встарь:
+Ночь, ледяная рябь канала,
+Аптека, улица, фонарь.</p>
+</body>
+</html>
